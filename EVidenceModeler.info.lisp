7e8f0dcde172b4aa738a395c7e94d28e16be3f11
Kaiyuan Han <hermit_han@163.com>
2025-04-23 12:26:49 +0800

    Initial commit

---
+++ EVidenceModeler.info.lisp
@@ -0,0 +1,7 @@
+(:name           "EVidenceModeler")
+(:repo           "taffish-org/EVidenceModeler")
+(:author         "KaiyuanHan")
+(:version        "2.1.0")
+(:intro          "The EVidenceModeler (aka EVM) software combines ab intio gene predictions and protein and transcript alignments into weighted consensus gene structures.")
+(:dependencies)
+(:functions      "EVidenceModeler")
---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-EVidenceModeler
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/EVidenceModeler/EVidenceModeler
+
+The EVidenceModeler (aka EVM) software combines ab intio gene predictions and protein and transcript alignments into weighted consensus gene structures.
